2714f75b6cff1390191e8f4e7499ab457470d923
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-10-10 15:05:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function chocolateBars() {
+function chocolateBars("snickers", "hundred grand", "kitkat", "skittles") {
+return
+}
+
    return
   }
   
