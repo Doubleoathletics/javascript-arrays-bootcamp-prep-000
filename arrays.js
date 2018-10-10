467125aba1e609c5efb73c6c065fb2e7b0915dba
Osazee S Omokaro <osazee.omokaro@yahoo.com>
2018-10-10 15:14:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
-var chocolateBars
-
-function chocolateBars(snickers, hundred grand, kitkat, skittles) {
+var chocolateBarsfunction chocolateBars(snickers, hundred grand, kitkat, skittles) {
+function chocolateBars("snickers", "hundred grand", "kitkat", "skittles") {
 return console.log(chocolateBars)
-
 }
 function addElementToBeginningOfArray(array, element) {
   addElementToBeginningOfArray.unshift([1],'foo')
